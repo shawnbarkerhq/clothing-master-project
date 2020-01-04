8ac1e07a28d32e847173691f878aea68adad1e11
shawnbarkerhq <shawn@shawnbarker.us>
2020-01-04 00:24:06 -0800

    added collection page with overview and selectors

--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { createStructuredSelector } from "reselect";
 
 import "./collections-overview.styles.scss";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import { connect } from "react-redux";
-import CollectionPreview from "../preview-collection/preview-collection.component";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
@@ -15,7 +15,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
