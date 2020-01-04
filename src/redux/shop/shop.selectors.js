230ff766e0665a45ee30fecf42e0f380dc903571
shawnbarkerhq <shawn@shawnbarker.us>
2020-01-03 23:03:36 -0800

    added directory to redux and shop, started structure of collections-overview

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,8 @@
+import { createSelector } from "reselect";
+
+const selectCollection = state => state.shop;
+
+export const selectCollections = createSelector(
+  [selectCollection],
+  shop => shop.collections
+);
